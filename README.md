fab1fd6e9d9009304a72404b5778653cc125695e
Pushkar Raj <px86@protonmail.com>
2025-06-21 20:51:19 +0530

    Add dependencies and setup a bot

---
+++ README.md
@@ -0,0 +1,3 @@
+# Fitness Tracker Telegram Bot
+
+I am creating a personal telegram bot that will help me keep track of my fitness, especially my diet.
--- main.py
+++ main.py
@@ -1,6 +1,38 @@
-def main():
-    print("Hello from fitness-tracker-bot!")
+import os
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class BotTokenUnavailableException(Exception):
+    """Exception thrown when telegram bot token is not available."""
+
+
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+if not TOKEN:
+    raise BotTokenUnavailableException(
+        "TELEGRAM_BOT_TOKEN environment variable is not set"
+    )
+
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!"
+    )
 
 
 if __name__ == "__main__":
-    main()
+    application = ApplicationBuilder().token(TOKEN).build()
+
+    start_handler = CommandHandler("start", start)
+    application.add_handler(start_handler)
+
+    application.run_polling()
